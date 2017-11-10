84fd47a6f86492005005537dc9af9362a40b1ab8
Leandro Segovia <ldlsegovia@gmail.com>
2017-11-10 18:33:52 -0300

    feat(): add ParsedItem class
    to hold parsed data with idx and errors array

--- lib/parxer/structs/parsed_item.rb
+++ lib/parxer/structs/parsed_item.rb
@@ -1,7 +1,19 @@
 class Parxer::ParsedItem
+  extend Forwardable
+
   attr_reader :idx
+
+  def_delegator :errors, :add_error
 
   def initialize(idx: nil)
     @idx = idx
+  end
+
+  def add_error(attribute_name, error)
+    errors.add_error(attribute_name, error)
+  end
+
+  def errors
+    @errors ||= Parxer::ItemErrors.new
   end
 end
--- spec/lib/parxer/structs/parsed_item_spec.rb
+++ spec/lib/parxer/structs/parsed_item_spec.rb
@@ -1,4 +1,24 @@
 require "spec_helper"
 
 describe Parxer::ParsedItem do
+  let(:params) { { idx: 1 } }
+  let(:item) { described_class.new(params) }
+
+  describe "#initialize" do
+    it { expect(item.idx).to eq(1) }
+    it { expect(item.errors).to be_a(Parxer::ItemErrors) }
+  end
+
+  describe "#add_error" do
+    let(:errors) { double(add_error: true) }
+    let(:attribute_name) { double }
+    let(:error) { double }
+
+    before do
+      expect(item).to receive(:errors).and_return(errors)
+      expect(errors).to receive(:add_error).with(attribute_name, error).and_return(true)
+    end
+
+    it { expect(item.add_error(attribute_name, error)).to eq(true) }
+  end
 end
