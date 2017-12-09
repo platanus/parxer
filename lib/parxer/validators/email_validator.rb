71037ecc6811078e67182889f88dfdd2c8adf575
Leandro Segovia <ldlsegovia@gmail.com>
2017-12-09 16:35:12 -0300

    feat(Validators): add email validator

---
+++ lib/parxer/validators/email_validator.rb
@@ -0,0 +1,10 @@
+# rubocop:disable Metrics/LineLength
+class Parxer::EmailValidator < Parxer::BaseValidator
+  EMAIL_REGEXP = %r{\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z}
+
+  def validate
+    v = context.value.to_s
+    return true if v.blank?
+    !!(v =~ EMAIL_REGEXP)
+  end
+end
---
+++ spec/lib/parxer/validators/email_validator_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+describe Parxer::EmailValidator do
+  let(:value) { "leandro@platan.us" }
+  let(:config) { { context: ctx } }
+  let(:ctx) { double(value: value) }
+
+  subject { described_class.new(config) }
+
+  it { expect(subject.id).to eq(:email) }
+  it { expect(subject.config).to eq(config) }
+
+  describe "#validate" do
+    let(:execute) { subject.validate }
+
+    it { expect(execute).to eq(true) }
+
+    context "with blank value" do
+      let(:value) { "" }
+
+      it { expect(execute).to eq(true) }
+    end
+
+    context "with invalid email" do
+      let(:value) { "invalid" }
+
+      it { expect(execute).to eq(false) }
+    end
+
+    context "with invalid email" do
+      let(:value) { "leandro@" }
+
+      it { expect(execute).to eq(false) }
+    end
+
+    context "with invalid email" do
+      let(:value) { "leandro@@platan.us" }
+
+      it { expect(execute).to eq(false) }
+    end
+
+    context "with invalid email" do
+      let(:value) { "@platan.us" }
+
+      it { expect(execute).to eq(false) }
+    end
+  end
+end
