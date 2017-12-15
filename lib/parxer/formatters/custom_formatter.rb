88af6a6a9ea53381fd3b9eb932fd4ac2d8b0c708
Leandro Segovia <ldlsegovia@gmail.com>
2017-12-15 17:43:05 -0300

    feat(Formatters): add custom formatter

---
+++ lib/parxer/formatters/custom_formatter.rb
@@ -0,0 +1,9 @@
+class Parxer::CustomFormatter < Parxer::BaseFormatter
+  def format_value(_v)
+    if !config[:formatter_proc].is_a?(Proc)
+      raise Parxer::FormatterError.new("'formatter_proc' needs to be a Proc")
+    end
+
+    instance_eval(&config[:formatter_proc])
+  end
+end
---
+++ spec/lib/parxer/formatters/custom_formatter_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+describe Parxer::CustomFormatter do
+  let(:value) { "Hello" }
+  let(:ctx) { double(value: value) }
+
+  let(:formatter_proc) do
+    Proc.new do
+      "#{value} Platanus!"
+    end
+  end
+
+  let(:formatter_config) { { formatter_proc: formatter_proc } }
+  let(:config) { formatter_config.merge(context: ctx) }
+
+  subject { described_class.new(config) }
+
+  it { expect(subject.config).to eq(formatter_config) }
+
+  describe "#apply" do
+    it { expect(subject.apply).to eq("Hello Platanus!") }
+
+    context "with blank value" do
+      let(:value) { "" }
+
+      it { expect(subject.apply).to be_nil }
+    end
+
+    context "with invalid proc" do
+      let(:formatter_proc) { "not a proc" }
+
+      it { expect { subject.apply }.to raise_error(Parxer::FormatterError, /'formatter_proc'/) }
+    end
+  end
+end
