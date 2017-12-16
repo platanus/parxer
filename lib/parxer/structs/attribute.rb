7d55445c45e0a14db85c504c6a378081a09e0f1e
Leandro Segovia <ldlsegovia@gmail.com>
2017-12-16 15:24:03 -0300

    feat(Attribute): add method to set formatter

--- lib/parxer/structs/attribute.rb
+++ lib/parxer/structs/attribute.rb
@@ -1,7 +1,7 @@
 class Parxer::Attribute
   extend Forwardable
 
-  attr_reader :id, :name, :validators
+  attr_reader :id, :name, :validators, :formatter
 
   def_delegators :validators, :add_validator
 
@@ -9,5 +9,9 @@ class Parxer::Attribute
     @id = id.to_sym
     @name = name
     @validators = Parxer::Validators.new
+  end
+
+  def add_formatter(formatter_name, config = {}, &block)
+    @formatter = Parxer::FormatterBuilder.build(formatter_name, config, &block)
   end
 end
--- spec/lib/parxer/structs/attribute_spec.rb
+++ spec/lib/parxer/structs/attribute_spec.rb
@@ -10,4 +10,23 @@ describe Parxer::Attribute do
     it { expect(subject.name).to eq(params[:name]) }
     it { expect(subject.validators).to eq([]) }
   end
+
+  describe "#add_formatter" do
+    def add_formatter
+      subject.add_formatter(formatter_name, formatter_config, &formatter_proc)
+    end
+
+    let(:formatter_name) { :string }
+    let(:formatter_config) { double }
+    let(:formatter_proc) { Proc.new {} }
+    let(:formatter) { double }
+
+    before do
+      expect(Parxer::FormatterBuilder).to receive(:build).with(
+        formatter_name, formatter_config, &formatter_proc
+      ).and_return(formatter)
+    end
+
+    it { expect { add_formatter }.to change(subject, :formatter).from(nil).to(formatter) }
+  end
 end
