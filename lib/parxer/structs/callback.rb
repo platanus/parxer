bc6810594cbd75fc1f6ec1784a6c5d6debf9c965
Leandro Segovia <ldlsegovia@gmail.com>
2018-02-01 18:17:24 -0300

    feat(Structs): add Callback object

---
+++ lib/parxer/structs/callback.rb
@@ -0,0 +1,20 @@
+class Parxer::Callback
+  include Parxer::Context
+
+  attr_reader :type, :action, :config
+
+  def initialize(type: nil, action: nil, config: {})
+    @type = type.to_sym
+    @context = config.delete(:context)
+    @config = config
+    @action = action
+  end
+
+  def run
+    if action.is_a?(Proc)
+      instance_eval(&action)
+    else
+      context.send(action)
+    end
+  end
+end
---
+++ spec/lib/parxer/structs/callback_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe Parxer::Callback do
+  let(:type) { "type" }
+  let(:action) { double }
+  let(:method_response) { "platanus" }
+  let(:ctx) { double(:ctx, some_method: method_response) }
+  let(:config) { { context: ctx } }
+
+  let(:params) do
+    {
+      type: type,
+      action: action,
+      config: config
+    }
+  end
+
+  subject { described_class.new(params) }
+
+  describe "#initialize" do
+    it { expect(subject.type).to eq(type.to_sym) }
+    it { expect(subject.action).to eq(action) }
+    it { expect(subject.context).to eq(ctx) }
+    it { expect(subject.config).to eq({}) }
+  end
+
+  describe "#run" do
+    context "with Proc action" do
+      let(:action) do
+        Proc.new do
+          "hello #{some_method}!"
+        end
+      end
+
+      it { expect(subject.run).to eq("hello platanus!") }
+    end
+
+    context "with method action" do
+      let(:action) { :some_method }
+
+      it { expect(subject.run).to eq(method_response) }
+    end
+  end
+end
