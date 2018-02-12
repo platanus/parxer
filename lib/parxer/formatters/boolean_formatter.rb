bd90542bdc424c2af20dbfba9a176d345314139f
Leandro Segovia <ldlsegovia@gmail.com>
2018-02-11 21:01:12 -0300

    fix(BooleanFormatter): add missing values

--- lib/parxer/formatters/boolean_formatter.rb
+++ lib/parxer/formatters/boolean_formatter.rb
@@ -1,8 +1,8 @@
 module Parxer
   module Formatter
     class Boolean < Base
-      TRUE_OPTIONS = ["true", "t", "1"]
-      FALSE_OPTIONS = ["false", "f", "0"]
+      TRUE_OPTIONS = ["true", "t", "1", "1.0"]
+      FALSE_OPTIONS = ["false", "f", "0", "0.0"]
 
       def format_value(v)
         return true if TRUE_OPTIONS.include?(v)
